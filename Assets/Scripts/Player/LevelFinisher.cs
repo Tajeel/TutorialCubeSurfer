4d61edb15fd8fb67f82c0d62dd50dc6258c558ef
Muhammad Tajeel Zia <tajeel@revolvinggames.com>
2021-11-09 12:10:52 +0500

    improved UI, colours, multiplierFloors, animations

---
+++ Assets/Scripts/Player/LevelFinisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFinisher : MonoBehaviour
+{
+    private bool _isActive;
+    
+    private void Start()
+    {
+        _isActive = true;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!_isActive)
+        {
+            return;
+        }
+
+        if (other.gameObject.layer == Constants.Layers.Barrier)
+        {
+            Handheld.Vibrate();
+            // interact with barriers
+            if (other.transform.CompareTag(Constants.Tags.Wall))
+            {
+                AudioManager.Instance.PlaySound((int) AudioManager.AudioClipsEnum.CollidedWithWall);
+                EventManager.TriggerEvent(Constants.Events.LevelStateParamEnum, Constants.LevelState.Failed);
+                _isActive = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Hover.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    [SerializeField] private float hoverSpeed;
+    [SerializeField] private float timeToHover;
+    private bool _hoverUp;
+
+    private void FixedUpdate()
+    {
+        StartCoroutine(_hoverUp ? HoverUp() : HoverDown());
+    }
+
+    private IEnumerator HoverUp()
+    {
+        transform.Translate(Vector3.up * hoverSpeed * Time.fixedDeltaTime);
+        yield return new WaitForSeconds(timeToHover);
+        _hoverUp = false;
+    }
+    
+    private IEnumerator HoverDown()
+    {
+        transform.Translate(Vector3.down * hoverSpeed * Time.fixedDeltaTime);
+        yield return new WaitForSeconds(timeToHover);
+        _hoverUp = true;
+    }
+}
